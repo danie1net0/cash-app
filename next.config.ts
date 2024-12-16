a2d6a54fd417221b7c72124d112c2ed6bc7374f0
Daniel Neto <danielneto314@gmail.com>
2024-12-16 15:38:06 -0300

    chore(next.config.ts): configura variável de ambiente da API

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  env: {
+    apiUrl: process.env.API_URL,
+  }
 };
 
 export default nextConfig;
